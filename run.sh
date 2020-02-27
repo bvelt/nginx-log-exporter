f0d5fece9be7e8fbdbae24fac1f605ed47bc087b
Brent Velthoen <brent.velthoen@gmail.com>
2020-02-27 08:23:56 -0800

    run docker detached

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s"
 docker build -t betteries/nginx-log-exporter:latest .
-docker run -p 9999:9999 -v /var/log/nginx:/nginxlogs betteries/nginx-log-exporter:latest
+docker run -d -p 9999:9999 -v /var/log/nginx:/nginxlogs betteries/nginx-log-exporter:latest
